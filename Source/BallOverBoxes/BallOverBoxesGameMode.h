c9dbb158907e67bff6d324d9c9c639b00be9568f
OLLIS-LABTOP\okram <o.kramm@hotmail.de>
2016-11-22 15:14:58 +0100

    added Unreal project

---
+++ Source/BallOverBoxes/BallOverBoxesGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BallOverBoxesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLOVERBOXES_API ABallOverBoxesGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
